369e44382a242493a25a8edaae630ec373c3fc22
JounQin <admin@1stg.me>
2021-04-26 18:15:01 +0800

    chore: improve type coverage

--- packages/markup/src/worker.ts
+++ packages/markup/src/worker.ts
@@ -2,7 +2,7 @@ import { exec } from 'markuplint'
 import { runAsWorker } from 'synckit'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
-runAsWorker(async options => {
+runAsWorker(async (options: Parameters<typeof exec>[0]) => {
   const results = await exec(options)
   return results.map(
     ({ results, filePath, sourceCode, fixedCode, parser, locale }) => ({
