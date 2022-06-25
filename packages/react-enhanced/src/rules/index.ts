dcd65db6f16e754bf88821e9476f0539ef35814f
JounQin <admin@1stg.me>
2022-06-26 00:52:06 +0800

    feat: add placeholder package for react-enhanced
    close #71

---
+++ packages/react-enhanced/src/rules/index.ts
@@ -0,0 +1,8 @@
+import { preferObservable } from './prefer-observable'
+
+export const rules = {
+  'prefer-observable': preferObservable,
+  preferObservable,
+}
+
+export { preferObservable } from './prefer-observable'
